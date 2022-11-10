95fd35363ef1305c366f188a1449c651c214b55f
BuildTools <unconfigured@null.spigotmc.org>
2022-11-10 00:05:29 -0500

    SMall changes

--- main.py
+++ main.py
@@ -18,26 +18,26 @@ import srtParser
 # actually cool code
 
 # scrape content
-#article_link = input("What do you wanna steal: ")
-## returns a dictionary with title and body values
-#print("Scraping Content...")
-#og_article_content = scraper.scrape(article_link)
+article_link = input("What do you wanna steal: ")
+# returns a dictionary with title and body values
+print("Scraping Content...")
+og_article_content = scraper.scrape(article_link)
 #
-## rewrite content, just sends article content over to an AI rewriter, also saves the rewritten article to a file
-## once again returns a dictionary, but with the rewritten contents
-#print("Rewriting Content...")
-#wr_article_content = rewriter.rewrite(og_article_content)
-#
-## make the text to speech audio files of our content and save them, returns dict of where the audio files are
-#print("Converting text to audio...")
-#audio_file_locations = speech.speak(wr_article_content)
+# rewrite content, just sends article content over to an AI rewriter, also saves the rewritten article to a file
+# once again returns a dictionary, but with the rewritten contents
+print("Rewriting Content...")
+wr_article_content = rewriter.rewrite(og_article_content)
+
+# make the text to speech audio files of our content and save them, returns dict of where the audio files are
+print("Converting text to audio...")
+audio_file_locations = speech.speak(wr_article_content)
 
 # checks and ajusts the captions to make sure all of the words are accurate
 #checker.check(wr_article_file_location, json_caption_data)
 
 # compile all of the cool stuff we just made into one dope video
-#print("Compiling into pre caption video...")
-#video_file_location = compiler.compile()
+print("Compiling into pre caption video...")
+video_file_location = compiler.compile()
 
 # make the captions based off of the video, it saves them to a file as well, returns dictionary with json data of the captions
 print("Getting speech to text data of video...")
--- srtParser.py
+++ srtParser.py
@@ -1,46 +1,45 @@
 def pasre(srt_data):
     #with open('./balbla.srt', 'r') as f:
     #    srt_data = f.read()
-
     srt_data = srt_data.split("\n\n")
     finished_segments = []
     for segment in srt_data:
-        print(segment)
-        # split the string by line break
-        segment = segment.split("\n")
-        # split the start time and end time
-        time_split = segment[1].split(" --> ")
-        print(time_split)
-        # get the start time in seconds
-        start_split = time_split[0].split(":")
-        print(start_split)
-        start_hours = int(start_split[0])
-        start_hours = start_hours*3600
-        start_minutes = int(start_split[1])
-        start_minutes = start_minutes*60
-        start_seconds = start_split[2]
-        start_seconds = start_seconds.replace(",", "." )
-        start_seconds = float(start_seconds)
-        start_time_seconds = start_hours + start_minutes + start_seconds
-        # get the end time in seconds
-        end_split = time_split[1].split(":")
-        end_hours = int(end_split[0])
-        end_hours = end_hours*3600
-        end_minutes = int(end_split[1])
-        end_minutes = end_minutes*60
-        end_seconds = end_split[2]
-        end_seconds = end_seconds.replace(",", "." )
-        end_seconds = float(end_seconds)
-        end_time_seconds = end_hours + end_minutes + end_seconds
-        # get the duration in seconds
-        duration = end_time_seconds - start_time_seconds
-        # bild back the content of the captions
-        content = ""
-        for line in segment[1:]:
-            content = content + line + "\n"
-        # create and append a dictionary to the finished)segments list
-        full = {"Start": start_time_seconds, "End": end_time_seconds, "Duration": duration, "Content": content}
-        finished_segments.append(full)
-    print(finished_segments)
+        if len(segment) > 0:
+            # split the string by line break
+            segment = segment.split("\n")
+            # split the start time and end time
+            time_split = segment[1].split(" --> ")
+
+            # get the start time in seconds
+            start_split = time_split[0].split(":")
+
+            start_hours = int(start_split[0])
+            start_hours = start_hours*3600
+            start_minutes = int(start_split[1])
+            start_minutes = start_minutes*60
+            start_seconds = start_split[2]
+            start_seconds = start_seconds.replace(",", "." )
+            start_seconds = float(start_seconds)
+            start_time_seconds = start_hours + start_minutes + start_seconds
+            # get the end time in seconds
+            end_split = time_split[1].split(":")
+            end_hours = int(end_split[0])
+            end_hours = end_hours*3600
+            end_minutes = int(end_split[1])
+            end_minutes = end_minutes*60
+            end_seconds = end_split[2]
+            end_seconds = end_seconds.replace(",", "." )
+            end_seconds = float(end_seconds)
+            end_time_seconds = end_hours + end_minutes + end_seconds
+            # get the duration in seconds
+            duration = end_time_seconds - start_time_seconds
+            # bild back the content of the captions
+            content = ""
+            for line in segment[2:]:
+                content = content + line + "\n"
+            # create and append a dictionary to the finished)segments list
+            full = {"Start": start_time_seconds, "End": end_time_seconds, "Duration": duration, "Content": content}
+            finished_segments.append(full)
+    #print(finished_segments)
 
     return finished_segments
